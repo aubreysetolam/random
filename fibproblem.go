25a6811eaf301370a15e37adf59529185b084ee5
aubreysetolam <ad.r.sl213@gmail.com>
2019-05-25 21:56:32 +0800

    Update fibproblem.go

--- fibproblem.go
+++ fibproblem.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 世界")
+  var ind int 
+  fmt.Scan(&ind)
+  fmt.Print(doFib(ind))
 }
+
+func doFib(index int) int{
+  var left, right int = 0,1
+  for x := 1; x < index; x++{
+    right, left = right + left, right
+  }
+  return right
+}
+
+//func recFib(index int)
